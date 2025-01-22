d3bc62ae610d9208ff3077723b96627affd64467
juhid30 <55.juhideore@gmail.com>
2025-01-22 20:52:51 +0530

    Test

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,22 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import Desktop from './components/Desktop'
-import Chrome from './components/Chrome'
-import MeetHome from './components/MeetHome'
-import MeetScreen from './components/MeetScreen'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import Desktop from "./components/Desktop";
+import Chrome from "./components/Chrome";
+import MeetHome from "./components/MeetHome";
+import MeetScreen from "./components/MeetScreen";
+import Test from "./components/Test";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
-   <>
-      <MeetScreen/>
-   </>
-  )
+    <>
+      <MeetScreen />
+      <Test />
+    </>
+  );
 }
 
-export default App
+export default App;
