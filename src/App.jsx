393da8c0074d766c65c3d156720865550bf668ab
jashrashne <jash.rashne@gmail.com>
2025-01-22 23:42:28 +0530

    push meet

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,31 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import Desktop from './components/Desktop'
-import Chrome from './components/Chrome'
-import MeetHome from './components/MeetHome'
-import MeetScreen from './components/MeetScreen'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import Desktop from "./components/Desktop";
+import Chrome from "./components/Chrome";
+import MeetDesktop from "./components/GMeet/MeetDesktop";
+import MeetChrome from "./components/GMeet/MeetChrome";
+import MeetHome from "./components/GMeet/MeetHome";
+import MeetScreem from "./components/GMeet/MeetScreem";
+
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
-   <>
-      <MeetScreen/>
-   </>
-  )
+    <>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Desktop />} />
+          <Route path="/meet-desktop" element={<MeetDesktop />} />
+          <Route path="/meet-chrome" element={<MeetChrome />} />
+          <Route path="/meet-home" element={<MeetHome />} />
+          <Route path="/meet-screen" element={<MeetScreen />} />
+        </Routes>
+      </Router>
+    </>
+  );
 }
 
-export default App
+export default App;
